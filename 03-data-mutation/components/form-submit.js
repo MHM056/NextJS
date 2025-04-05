e58116ac92008c05929266f4db0e5828912cef0c
Mihail <mihailmarholev@gmail.com>
2025-04-05 21:41:37 +0300

    Updated form-submit component

--- 03-data-mutation/components/form-submit.js
+++ 03-data-mutation/components/form-submit.js
@@ -1,14 +1,4 @@
-"use client";
-
-import { useActionState } from "react";
-
 export default function FormSubmit() {
-    const status = useActionState();
-    console.log(status);
-    
-    if(status.pending) {
-        return <p>Creating post...</p>;
-    }
 
     return (
         <>
