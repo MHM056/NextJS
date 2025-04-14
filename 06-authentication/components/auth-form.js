b3d3cbc460c19cef783388e65294b4a73e5f9df9
Mihail <mihailmarholev@gmail.com>
2025-04-14 19:03:10 +0300

    Updated auth-form component

--- 06-authentication/components/auth-form.js
+++ 06-authentication/components/auth-form.js
@@ -1,10 +1,10 @@
 "use client";
-import { signup } from '@/actions/auth-actions';
+import { auth, signup } from '@/actions/auth-actions';
 import Link from 'next/link';
 import { useActionState } from 'react';
 
-export default function AuthForm() {
-  const [formState, formAction] = useActionState(signup, {});
+export default function AuthForm({ mode }) {
+  const [formState, formAction] = useActionState(auth.bind(null, mode), {});
 
   return (
     <form action={formAction} id="auth-form">
@@ -24,11 +24,12 @@ export default function AuthForm() {
           {Object.keys(formState.errors).map((error) => <li key={error}>{formState.errors[error]}</li>)}
         </ul>)}
         <button type="submit">
-          Create Account
+          {mode === 'login' ? 'Login' : 'Create Account'}
         </button>
       </p>
       <p>
-        <Link href="/">Login with existing account.</Link>
+        {mode === 'login' && <Link href="/?mode=signup">Create an account.</Link>}
+        {mode === 'signup' && <Link href="/?mode=login">Login with existing account.</Link>}
       </p>
     </form>
   );
